1f5e687b69cae31c5692fa4e4bd2d24ffaaf962e
DeLL <cesarcafu@hotmail.com>
2023-12-19 12:03:58 +0100

    MejoraLogin

---
+++ app/src/main/java/com/example/myapplication/MainACtivity2.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        // Puedes realizar cualquier configuración adicional necesaria en MainActivity2 aquí
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val editTextEmail: TextInputEditText = findViewById(R.id.editTextEmail)
+        val editTextPassword: TextInputEditText = findViewById(R.id.editTextPassword)
+        val buttonLogin: Button = findViewById(R.id.buttonLogin)
+
+
+        buttonLogin.setOnClickListener {
+            val email: String = editTextEmail.text.toString().trim()
+            val password: String = editTextPassword.text.toString().trim()
+
+            if (isValidInput(email, password)) {
+
+                val intent = Intent(this, MainActivity2::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, getString(R.string.error_empty_fields), Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+    }
+
+    private fun isValidInput(email: String, password: String): Boolean {
+        return email.isNotEmpty() && password.isNotEmpty()
     }
 }
+
